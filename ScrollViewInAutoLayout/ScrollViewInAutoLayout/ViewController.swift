a6b3764e7d56d0c8c85db2a5db772cbe8438c377
XuJian <xcoderj@gmail.com>
2017-02-13 00:40:33 +0800

    Autolayout下使用scrollview

---
+++ ScrollViewInAutoLayout/ScrollViewInAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewInAutoLayout
+//
+//  Created by xujian on 2017/2/12.
+//  Copyright © 2017年 xujian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
